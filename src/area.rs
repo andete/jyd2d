e5f53c66626107c95d03232a76ecd4abfcd0d35a
Joost Yervante Damad <joost@damad.be>
2019-11-17 10:27:59 +0100

    basic SVG Element test

--- src/area.rs
+++ src/area.rs
@@ -1,4 +1,28 @@
+use crate::coordinate::Coordinate;
+use svg::node::element::Element;
+use svg::node::element::path::Data;
+use svg::node::element::Path;
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct Area {
     pub corners: Vec<Coordinate>,
+}
+
+impl Into<Element> for Area {
+    fn into(self) -> Element {
+
+        let data = Data::new();
+        assert!(self.corners.len() > 2);
+        data.move_to((corners[0].x, corners[0].y));
+        for corner in self.corners[1..] {
+            data.line_to((corner.x, corner.y));
+        }
+        data.close();
+
+        Path::new()
+            .set("fill", "none")
+            .set("stroke", "black")
+            .set("stroke-width", 0.25)
+            .set("d", data)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 // (c) 2019 Joost Yervante Damad <joost@damad.be>
 
+pub mod area;
 pub mod coordinate;
 pub mod matrix2;
 pub mod matrix3;
