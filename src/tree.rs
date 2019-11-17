2c2740710e99e2b4ec31e42d76aaf200e4d55198
Joost Yervante Damad <joost@damad.be>
2019-11-17 15:30:55 +0100

    make crown diameter optional

--- src/tree.rs
+++ src/tree.rs
@@ -13,7 +13,7 @@ pub struct Tree {
     pub name: String,
     pub species: String,
     pub trunk_diameter: f64,
-    pub crown_diameter: f64,
+    pub crown_diameter: Option<f64>,
     pub location: Coordinate,
     pub label_location: Coordinate,
 }
@@ -22,20 +22,26 @@ impl Into<Group> for Tree {
     fn into(self) -> Group {
         let label: TextElement = Label::new(self.label_location, &self.name).into();
         let desc = Description::new().add(Text::new(format!("{}", self.species)));
-        let title = Title::new().add(Text::new(format!("Boom {}", self.name)));
-        let crown = Circle::new()
+        let title = Title::new().add(Text::new(format!("Tree {}", self.name)));
+        let crown = self.crown_diameter.map(|d| Circle::new()
             .set("fill", Color::Green.to_string())
             .set("stroke", Color::DarkGreen.to_string())
-            .set("r", self.crown_diameter)
-            .set("id", format!("tree-crown-{}", self.name));
+            .set("r", d/2.0));
         let trunk = Circle::new()
             .set("fill", Color::Brown.to_string())
             .set("stroke", Color::Maroon.to_string())
-            .set("r", self.trunk_diameter)
-            .set("id", format!("tree-trunk-{}", self.name));
-        Group::new().add(label).add(crown).add(trunk)
+            .set("r", self.trunk_diameter/2.0);
+        let group = Group::new().add(label);
+        let group = match crown {
+            Some(c) => group.add(c),
+            None => group,
+        };
+        group.add(trunk)
             .set("stroke-width", 0.2)
             .set("cx", self.location.x)
-            .set("cy", -self.location.y).add(desc).add(title)
+            .set("cy", -self.location.y)
+            .set("id", format!("tree-{}", self.name))
+            .add(desc)
+            .add(title)
     }
 }
